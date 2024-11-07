e432ef99f47c2e4339bd14d7d789fbe455a79138
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-11-06 20:23:12 -0500

    updating the repo test files with first test case

--- src/test/java/com/thinkconstructive/restdemo/service/impl/CloudVendorServiceImplTest.java
+++ src/test/java/com/thinkconstructive/restdemo/service/impl/CloudVendorServiceImplTest.java
@@ -6,25 +6,25 @@ import com.thinkconstructive.restdemo.service.CloudVendorService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 class CloudVendorServiceImplTest {
 
     @Mock
     private CloudVendorRepository cloudVendorRepository;
+
     private CloudVendorService cloudVendorService;
+
     AutoCloseable autoCloseable;
+
     CloudVendor cloudVendor;
 
     @BeforeEach
@@ -32,7 +32,8 @@ class CloudVendorServiceImplTest {
         autoCloseable = MockitoAnnotations.openMocks(this);
         cloudVendorService = new CloudVendorServiceImpl(cloudVendorRepository);
         cloudVendor = new CloudVendor("1", "Amazon",
-                "USA", "xxxxx");
+                "USA", "43575365253");
+
     }
 
     @AfterEach
@@ -46,65 +47,40 @@ class CloudVendorServiceImplTest {
         mock(CloudVendorRepository.class);
 
         when(cloudVendorRepository.save(cloudVendor)).thenReturn(cloudVendor);
-        assertThat(cloudVendorService.createCloudVendor(cloudVendor)).isEqualTo("Success");
-
+        assertThat(cloudVendorService.createCloudVendor(cloudVendor))
+                .isEqualTo("Success");
     }
 
     @Test
-    void testUpdateCloudVendor() {
-        mock(CloudVendor.class);
+    void updateCloudVendor() {
         mock(CloudVendorRepository.class);
-
+        mock(CloudVendor.class);
         when(cloudVendorRepository.save(cloudVendor)).thenReturn(cloudVendor);
-        assertThat(cloudVendorService.updateCloudVendor(cloudVendor)).isEqualTo("Success");
+        assertThat(cloudVendorService.updateCloudVendor(cloudVendor))
+                .isEqualTo("Success");
     }
 
     @Test
-    void testDeleteCloudVendor() {
-        mock(CloudVendor.class);
-        mock(CloudVendorRepository.class, Mockito.CALLS_REAL_METHODS);
-
-        doAnswer(Answers.CALLS_REAL_METHODS).when(cloudVendorRepository)
-                .deleteById(any());
-        assertThat(cloudVendorService.deleteCloudVendor("1")).isEqualTo("Success");
+    void deleteCloudVendor() {
     }
 
     @Test
-    void testGetCloudVendor() {
-        mock(CloudVendor.class);
+    void getCloudVendor() {
         mock(CloudVendorRepository.class);
-
-        when(cloudVendorRepository.findById("1")).thenReturn(Optional.ofNullable(cloudVendor));
-
-        assertThat(cloudVendorService.getCloudVendor("1").getVendorName())
+        mock(CloudVendor.class);
+        when(cloudVendorRepository.findById("1"))
+                .thenReturn(Optional.ofNullable(cloudVendor));
+        assertThat(cloudVendorService.getCloudVendor("1")
+                .getVendorName())
                 .isEqualTo(cloudVendor.getVendorName());
+
     }
 
     @Test
-    void testGetByVendorName() {
-        mock(CloudVendor.class);
-        mock(CloudVendorRepository.class);
-
-        when(cloudVendorRepository.findByVendorName("Amazon")).
-                thenReturn(new ArrayList<CloudVendor>(Collections.singleton(cloudVendor)));
-
-        assertThat(cloudVendorService.getByVendorName("Amazon").get(0).getVendorId()).
-                isEqualTo(cloudVendor.getVendorId());
+    void getAllCloudVendors() {
     }
 
     @Test
-    void testGetAllCloudVendors() {
-        mock(CloudVendor.class);
-        mock(CloudVendorRepository.class);
-
-        when(cloudVendorRepository.findAll()).thenReturn(new ArrayList<CloudVendor>(
-                Collections.singleton(cloudVendor)
-        ));
-
-        assertThat(cloudVendorService.getAllCloudVendors().get(0).getVendorPhoneNumber()).
-                isEqualTo(cloudVendor.getVendorPhoneNumber());
-
+    void getByVendorName() {
     }
-
-
 }
